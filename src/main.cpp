4848770dffc49ccd6dfc6a84aa3fe7e047515684
Carlos DaLomba <carlos@cometpeakgames.net>
2023-06-01 02:27:08 -0400

    Added first example of multithreading in C++!

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,31 @@
 #include <iostream>
 #include <string>
+#include <future>
+#include <thread>
 
-using std::cout;
-using std::endl;
-using std::cin;
-using std::string;
-using std::getline;
+using namespace std;
+
+float testLongMethod() {
+    float result = 0;
+    for (int i = 0; i < 2000000000; i++) {
+        result += (float) i / 2;
+    }
+    result *= 0.2049;
+    return result;
+}
 
 int main() {
     cout << "Hello world!" << endl;
     
+    future<float> task = async(
+        []() { return testLongMethod(); }
+    );
+
     string input;
     getline(cin, input);
+
+    string result = to_string(task.get());
+    cout << "The result calculated was " << result << endl;
 
     cout << "You entered: " << input << endl;
 }
