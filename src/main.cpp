a1e5dae730832fff565b878c1c371dae602071f8
Carlos DaLomba <carlos@cometpeakgames.net>
2023-06-01 02:07:57 -0400

    Added demonstration of 1 line of user input

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <string>
 
 using std::cout;
 using std::endl;
+using std::cin;
+using std::string;
+using std::getline;
 
 int main() {
     cout << "Hello world!" << endl;
+    
+    string input;
+    getline(cin, input);
+
+    cout << "You entered: " << input << endl;
 }
